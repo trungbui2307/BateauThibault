fc67ff81734b4c264162652808e3a88d47e11e7c
toancqb <toantqcqb@gmail.com>
2022-01-12 00:09:29 +0100

    add page detailsProduct manageStock and menu Header

--- FrontendAngular/src/app/app.module.ts
+++ FrontendAngular/src/app/app.module.ts
@@ -5,16 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { HeaderComponent } from './features/header/header.component';
+import { DetailsProductComponent } from './pages/details-product/details-product.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { ManageStockComponent } from './pages/manage-stock/manage-stock.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    DetailsProductComponent,
+    ManageStockComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- FrontendAngular/src/app/features/header/header.component.ts
+++ FrontendAngular/src/app/features/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  public selectedIndex: number = 0;
+  //public backgroundColor: string = 'black';
+
+  public elements: any = [
+    {
+      id: 0,
+      title: "Home", 
+      path: "",
+      isClicked: true
+    },
+    {
+      id: 1,
+      title: "Details d'un produit", 
+      path: "detail",
+      isClicked: false
+    },
+    {
+      id: 2,
+      title: "Gerer les produits", 
+      path: "stock",
+      isClicked: false
+    }
+  ]
+
+  constructor(private route: Router) { }
 
   ngOnInit(): void {
   }
 
+  public onClickMenu(selectedIndex:number): void {
+    this.resetElements();
+    this.elements[selectedIndex].isClicked = true;
+    console.log(selectedIndex);
+
+    this.route.navigate([this.elements[selectedIndex].path])
+  }
+
+  private resetElements(): void {
+    for(let i = 0;i < 3; i++) {
+      this.elements[i].isClicked = false;
+    }
+  }
 }
