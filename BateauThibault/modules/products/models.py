cc869ca1fd72580ae4cabd40a5ed8aae98c5c6e7
trungbui2307 <tb23073010>
2022-01-12 16:19:42 +0100

    Finish modify discount price

--- BateauThibault/modules/products/models.py
+++ BateauThibault/modules/products/models.py
@@ -5,14 +5,14 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=20, verbose_name="Name of product", unique=True)
     category = models.IntegerField(default=5, verbose_name="Category", blank=False, null=False)
-    price = models.FloatField(default=10, verbose_name="Price", blank=False, null=False)
+    price = models.FloatField(default=0, verbose_name="Price", blank=False, null=False)
     unit = models.CharField(max_length=20, verbose_name="Unit")
     availability = models.BooleanField(default=True, verbose_name="Availability")
     sale = models.BooleanField(default=False, verbose_name="Sale")
-    discount = models.FloatField(default=10, verbose_name="Discount")
+    discount = models.FloatField(default=0, verbose_name="Discount")
     comments = models.TextField(verbose_name="Comment", blank=True, null=True)
     owner = models.CharField(max_length=20, verbose_name="Name of owner", blank=False, null=False)
-    quantityInStock = models.IntegerField(default=10, verbose_name="Stock quantity", blank=False, null=False)
+    quantityInStock = models.IntegerField(default=0, verbose_name="Stock quantity", blank=False, null=False)
 
     class Meta:
         ordering = ('name',)
--- BateauThibault/modules/products/urls.py
+++ BateauThibault/modules/products/urls.py
@@ -6,5 +6,6 @@ register_converter(converter.FloatUrlParameterConverter, 'float')
 urlpatterns = [
     path('infoproducts/', views.ProductListAPIView.as_view(), name='products_list'),
     path('infoproduct/<int:id>/', views.ProductRetrieveAPIView.as_view(), name='product_detail'),
-    path('putonsale/<int:id>/<float:newprice>/', views.ProductDiscountAPIView.as_view(), name='product_on_sale')
+    path('putonsale/<int:id>/<float:newprice>/', views.ProductDiscountAPIView.as_view(), name='product_on_sale'),
+    path('remove/<int:id>/', views.ProductRemoveAPIView.as_view(), name='product_sale_remove'),
 ]
