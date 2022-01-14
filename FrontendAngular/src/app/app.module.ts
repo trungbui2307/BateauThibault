23a5117c7233bf6a1775fd179c08bdcd93e46f51
Quan Nguyen <minhquan.nguyen.fr@gmail.com>
2022-01-14 13:14:08 +0100

    BAT-5 [DBO][UI] add manage stock page

--- FrontendAngular/src/app/app.module.ts
+++ FrontendAngular/src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -31,11 +34,14 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     FlexLayoutModule,
+    MatTableModule,
     MatCardModule,
     MatDividerModule,
     MatButtonModule,
+    MatInputModule,
     MatFormFieldModule,
     MatSelectModule,
+    MatSnackBarModule,
     HttpClientModule,
     BrowserAnimationsModule
   ],
--- FrontendAngular/src/app/pages/manage-stock/manage-stock.component.ts
+++ FrontendAngular/src/app/pages/manage-stock/manage-stock.component.ts
@@ -1,15 +1,121 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  Product,
+  ProductService,
+  UpdatedProduct,
+} from 'src/app/core/product.service';
+import {MatSnackBar} from '@angular/material/snack-bar';
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
 
 @Component({
   selector: 'app-manage-stock',
   templateUrl: './manage-stock.component.html',
-  styleUrls: ['./manage-stock.component.scss']
+  styleUrls: ['./manage-stock.component.scss'],
 })
 export class ManageStockComponent implements OnInit {
+  displayedColumns: string[] = [
+    'name',
+    'price',
+    'discount',
+    'quantity',
+    'quantity_sold',
+    'quantityInStock',
+    'comments',
+    'addStock',
+    'addPromo',
+  ];
+  dataSource: Product[] = [];
 
-  constructor() { }
+  updateData: UpdatedProduct[] = [];
+
+  constructor(private productService: ProductService, private _snackBar: MatSnackBar) {}
 
   ngOnInit(): void {
+    this.getProducts();
   }
 
+  public getProducts() {
+    this.productService.getProductsFromJson().subscribe(
+      (res: Product[]) => {
+        this.dataSource = res;
+      },
+      (err) => {
+        alert('API Get failed');
+      }
+    );
+  }
+
+  getDataSource(i: number) {
+    return this.dataSource.filter((d) => d.category === i);
+  }
+
+  getTableName(i: number) {
+    switch (i) {
+      case 0:
+        return 'Crustaces';
+      case 1:
+        return 'Poissons';
+      case 2:
+        return 'Fruit de mer';
+      default:
+        return 'Some Table';
+    }
+  }
+
+  addStock(elementId: number, addStockInput: string) {
+    const index = this.updateData.findIndex((d) => d.id === elementId);
+    if (index !== -1) {
+      this.updateData.splice(index, 1, {
+        ...this.updateData[index],
+        id: elementId,
+        quantityInStock: this.dataSource.find((d) => d.id === elementId)!.quantityInStock! + (+addStockInput),
+      });
+      return;
+    }
+    this.updateData.push({
+      id: elementId,
+      quantityInStock: this.dataSource.find((d) => d.id === elementId)!.quantityInStock! + (+addStockInput),
+    });
+  }
+
+  addPromo(elementId: number, addPromoInput: string) {
+    const index = this.updateData.findIndex((d) => d.id === elementId);
+    if (index !== -1) {
+      this.updateData.splice(index, 1, {
+        ...this.updateData[index],
+        id: elementId,
+        discount: Math.min(this.dataSource.find((d) => d.id === elementId)!.discount! + (+addPromoInput), 100),
+      });
+      return;
+    }
+    this.updateData.push({
+      id: elementId,
+      discount: Math.min(this.dataSource.find((d) => d.id === elementId)!.discount! + (+addPromoInput), 100),
+    });
+  }
+
+  openSnackBar(message: string, action: string) {
+    this._snackBar.open(message, action, {
+      duration: 2000
+    });
+  }
+
+  updateProducts() {
+    this.productService.updateProducts(this.updateData).subscribe((res: Product[]) => {
+      this.openSnackBar("Products updated successfully", "UPDATED!")
+      res.forEach(product => {
+        this.dataSource.splice(this.dataSource.findIndex(d => d.id === product.id), 1, {
+          ...product
+        })
+      });
+    }, (err) => {
+      this.openSnackBar(err, "ERROR!")
+    });
+  }
 }
