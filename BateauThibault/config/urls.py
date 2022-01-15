66cc27ed04d1bd4d380ff388258e53e337074e42
Quan Nguyen <minhquan.nguyen.fr@gmail.com>
2022-01-15 21:06:18 +0100

    Add auth login service + authorization

--- BateauThibault/config/urls.py
+++ BateauThibault/config/urls.py
@@ -26,6 +26,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('modules.products.urls')),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/login', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- FrontendAngular/src/app/core/product.service.ts
+++ FrontendAngular/src/app/core/product.service.ts
@@ -2,20 +2,26 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
+  public API_URL: string = 'http://localhost:8000/api/v1';
 
-  public API_URL: string = "http://localhost:8000/api/v1";
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization:
+        `Bearer ${localStorage.getItem('accessToken')}`,
+    }),
+  };
 
   public currentMenuTab: any = [true, false, false, false];
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   public resetMenuTabs(): void {
-    for(let i = 0;i < this.currentMenuTab.length; i++) {
+    for (let i = 0; i < this.currentMenuTab.length; i++) {
       this.currentMenuTab[i] = false;
     }
   }
@@ -26,81 +32,78 @@ export class ProductService {
   }
 
   public getProductsFromJson(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.API_URL + "/infoproducts"); // Get /inforproducts "../assets/data/products.json"
+    return this.http.get<Product[]>(this.API_URL + '/infoproducts', this.httpOptions); // Get /inforproducts "../assets/data/products.json"
   }
 
-  public putProduct(id: number, product: PutProductOnSale): Observable<PutProductOnSale> {
-    return this.http.put<PutProductOnSale>(this.API_URL + "/putonsale/" + id + "/", product,
-      {
-        headers: new HttpHeaders({
-          'Content-Type': 'application/json'
-        })
-      }
+  public putProduct(
+    id: number,
+    product: PutProductOnSale
+  ): Observable<PutProductOnSale> {
+    return this.http.put<PutProductOnSale>(
+      this.API_URL + '/putonsale/' + id + '/',
+      product,
+      this.httpOptions
     );
   }
 
   public updateProducts(products: UpdatedProduct[]): Observable<Product[]> {
-    console.log(products)
-    return this.http.put<Product[]>(this.API_URL + "/products/", products,
-      {
-        headers: new HttpHeaders({
-          'Content-Type': 'application/json'
-        })
-      }
+    console.log(products);
+    return this.http.put<Product[]>(this.API_URL + '/products/', products, this.httpOptions);
+  }
+
+  public putIncrementQuantityStockProduct(
+    id: number,
+    nbProduct: StockProduct
+  ): Observable<StockProduct> {
+    nbProduct.number = Number(nbProduct.number);
+    return this.http.put<StockProduct>(
+      this.API_URL + '/incrementStock/' + id + '/',
+      nbProduct,
+      this.httpOptions
     );
   }
 
-  public putIncrementQuantityStockProduct(id: number, nbProduct: StockProduct): Observable<StockProduct> {
-    nbProduct.number = Number(nbProduct.number)
-    return this.http.put<StockProduct>(this.API_URL + "/incrementStock/" + id + "/", nbProduct,
-      {
-        headers: new HttpHeaders({
-          'Content-Type': 'application/json'
-        })
-      }
-    );
-  }
-
-  public putDecrementQuantityStockProduct(id: number, nbProduct: StockProduct): Observable<StockProduct> {
-    nbProduct.number = Number(nbProduct.number)
-    return this.http.put<StockProduct>(this.API_URL + "/decrementStock/" + id + "/", nbProduct,
-      {
-        headers: new HttpHeaders({
-          'Content-Type': 'application/json'
-        })
-      }
+  public putDecrementQuantityStockProduct(
+    id: number,
+    nbProduct: StockProduct
+  ): Observable<StockProduct> {
+    nbProduct.number = Number(nbProduct.number);
+    return this.http.put<StockProduct>(
+      this.API_URL + '/decrementStock/' + id + '/',
+      nbProduct,
+      this.httpOptions
     );
   }
 }
 
 export interface Product {
-  id: number,
-  unit: string,
-  category: number,
-  name: string,
-  discount: number,
-  comments: string,
-  owner: string,
-  price: number,
-  price_selling: number,
-  price_on_sale: number,
-  sale: boolean,
-  availability: boolean,
-  quantity_in_stock: number,
-  quantity_sold: number,
+  id: number;
+  unit: string;
+  category: number;
+  name: string;
+  discount: number;
+  comments: string;
+  owner: string;
+  price: number;
+  price_selling: number;
+  price_on_sale: number;
+  sale: boolean;
+  availability: boolean;
+  quantity_in_stock: number;
+  quantity_sold: number;
 }
 
 export interface UpdatedProduct {
-  id: number,
-  price_selling?: number,
-  discount?: number,
-  quantity_in_stock?: number,
+  id: number;
+  price_selling?: number;
+  discount?: number;
+  quantity_in_stock?: number;
 }
 
 export interface PutProductOnSale {
-  discount: number,
+  discount: number;
 }
 
 export interface StockProduct {
-  number: number,
+  number: number;
 }
